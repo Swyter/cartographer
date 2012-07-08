57a22508bd8f1c8abe3e1d2bf3a09ec41f68d971
Swyter <Swyter@Portatil>
2012-07-08 22:08:01 +0200

    *fixed SOIL interface, textures loading fine now *Mount&Blade fonts loading fine. Test case with the "a" letter.

--- cartographer.lua
+++ cartographer.lua
@@ -1,7 +1,10 @@
 local lujgl = require "lujgl"
-local gl, glu = lujgl.gl, lujgl.glu
+local gl, glu, ffi = lujgl.gl, lujgl.glu, require "ffi"
+require "soil"
 
 lujgl.initialize("cartographer", 800, 600)
+
+fontdds = soil.loadTexture("res\\FONT_SWC.dds")
 
 gl.glMatrixMode(gl.GL_PROJECTION)
 glu.gluPerspective(60,lujgl.width / lujgl.height,0.01, 1000)
@@ -9,7 +12,23 @@ gl.glMatrixMode(gl.GL_MODELVIEW)
 glu.gluLookAt(0,0,5,
 	0,0,0,
 	0,1,0)
-
+	 
+	font = {
+		w=2048,
+		h=1024
+	}
+	
+	font.a = {
+		 code=97,
+		 page=0,
+		 u=787/font.w,
+		 v=1-(205/font.h),
+		 w=845/font.w,
+		 h=1-(258/font.h),
+		 preshift=-5,
+		 yadjust=50,
+		 postshift=47
+	}
 gl.glEnable(gl.GL_DEPTH_TEST)
 gl.glEnable(gl.GL_COLOR_MATERIAL)
 
@@ -24,7 +43,45 @@ lujgl.setRenderCallback(
 		gl.glClear(bit.bor(gl.GL_COLOR_BUFFER_BIT, gl.GL_DEPTH_BUFFER_BIT));
 		glu.gluPerspective(60,lujgl.width / lujgl.height,0.01, 1000)
 
-	end
+		-- 2D stuff
+		gl.glEnable(gl.GL_TEXTURE_2D)
+		gl.glBindTexture(gl.GL_TEXTURE_2D,fontdds)
+		lujgl.begin2D()
+		gl.glBegin(gl.GL_QUADS)
+		
+			--1
+			gl.glColor3d(1,0,0)
+			gl.glTexCoord2d(font.a.u,font.a.h)--(0,0)
+			gl.glVertex2i(  0,   0)
+			
+			--2
+			gl.glColor3d(0,1,0)
+			gl.glTexCoord2d(font.a.w,font.a.h)--(1,0)
+			gl.glVertex2i(550,   0)
+			
+			--3
+			gl.glColor3d(0,0,1)
+			gl.glTexCoord2d(font.a.w,font.a.v)--(1,1)
+			gl.glVertex2i(550, 550)
+			
+			--4
+			gl.glColor3d(0,0,0)
+			gl.glTexCoord2d(font.a.u,font.a.v)--(0,1)
+			gl.glVertex2i(  0, 550)
+			
+	--    v
+	--   4|      3
+	-- u--+-----+
+	--	  |     |
+	--	  |     |
+	--    +-----+--w
+	--   1      |2
+	--          h
+		gl.glEnd()
+		lujgl.end2D()
+		
+		lujgl.checkError()
+		end
 	)
 	
 lujgl.setEventCallback(function() print("event") end)
