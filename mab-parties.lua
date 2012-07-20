08b7772580ba0cd891fa1ae6ab2e01eb20bfb1ee
Swyter <Swyter@Portatil>
2012-07-20 18:17:59 +0200

    *Loads the right lines already, skip disabled/dummy/padding ones... See #5  Expanding the logic from previous commit

--- mab-parties.lua
+++ mab-parties.lua
@@ -1,24 +1,37 @@
 mab = mab or {}
 mab.parties = mab.parties or {}
-
+--[[
 mab.parties = {
       "taris"={name="Taris",    pos={15.66,90.63}, rot=135},
-  "coruscant"={name="Coruscant",pos={-30.5,34},    rot=135},
+  "coruscant"={name="Coruscant",pos={-30.5,34},    rot=135}
 }
+]]
 
 function mab.parties:load(filename)
-  print"not implemented yet"
+  print("@--start parsing parties"); s=0
+  for line in io.lines(filename) do
   
-  if not "#" and not "pf_disabled" then --avoid comments and filler entries
-    thingie=split("\((.+)\)\,")--isolate tuple somehow
-    tuple=split(thingie,",")
-    
-    mab.parties[tuple[1]]={
-      name=tuple[2], pos={tuple[10][1],tuple[10][2]}, rot=tuple[12] or 0
-    }
-    
+        local ltrim=line:match("%S.*") or "#"
+        local index=ltrim:sub(1,1)
+
+        if index ~= "#" then
+          if not line:find("pf_disabled") then --avoid comments and filler entries
+            --print(line)
+            s=s+1
+         -- thingie=split("\((.+)\)\,")--isolate tuple somehow
+         -- tuple=split(thingie,",")
+          
+         -- mab.parties[tuple[1]]={
+         -- name=tuple[2], pos={tuple[10][1],tuple[10][2]}, rot=tuple[12] or 0
+         -- }
+          end
+        end
   end
+  
+  print(string.format("   %d parties loaded...",s))
 end
+
+mab.parties:load("R:\\Juegos\\swconquest\\modules\\swconquest-msys\\module_parties.py")
 
 function mab.parties:save(filename)
   print"not implemented yet"
