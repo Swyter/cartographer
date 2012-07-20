d84e9763a8a5a2483df8c2c5acb52c01e389ae1b
Swyter <690187+Swyter@users.noreply.github.com>
2012-07-20 18:58:35 +0200

    *Only parse lines starting by tuple, cut, heal, order and serialize, shows display names in the log.  See #5.

--- mab-parties.lua
+++ mab-parties.lua
@@ -7,6 +7,32 @@ mab.parties = {
 }
 ]]
 
+
+function Split(str, delim, maxNb) --from <http://lua-users.org/wiki/SplitJoin> #Function: Split a string with a pattern, Take Three
+    -- Eliminate bad cases...
+    if string.find(str, delim) == nil then
+        return { str }
+    end
+    if maxNb == nil or maxNb < 1 then
+        maxNb = 0    -- No limit
+    end
+    local result = {}
+    local pat = "(.-)" .. delim .. "()"
+    local nb = 0
+    local lastPos
+    for part, pos in string.gfind(str, pat) do
+        nb = nb + 1
+        result[nb] = part
+        lastPos = pos
+        if nb == maxNb then break end
+    end
+    -- Handle the last field
+    if nb ~= maxNb then
+        result[nb + 1] = string.sub(str, lastPos)
+    end
+    return result
+end
+
 function mab.parties:load(filename)
   print("@--start parsing parties"); s=0
   for line in io.lines(filename) do
@@ -15,9 +41,16 @@ function mab.parties:load(filename)
         local index=ltrim:sub(1,1)
 
         if index ~= "#" then
-          if not line:find("pf_disabled") then --avoid comments and filler entries
-            --print(line)
-            s=s+1
+          if index=="(" and not line:find("pf_disabled") then --avoid comments and filler entries
+             
+             tuple=ltrim:sub(2,ltrim:find("%).*")) --remove possible comments from the right side
+             tuple = tuple:gsub("\"", ""):gsub("%(", ""):gsub("%)", "") --remove all the: "()
+
+             tuple = Split(tuple,",")
+              print(tuple[2]:gsub("_", " "))
+              s=s+1
+              
+              
          -- thingie=split("\((.+)\)\,")--isolate tuple somehow
          -- tuple=split(thingie,",")
           
