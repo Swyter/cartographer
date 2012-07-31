57c17ea2dda4d054ed3c33d2c8d9b87e71ad6292
Swyter <Swyter@Portatil>
2012-07-31 15:25:26 +0200

    *Sanitize strings with brain *remove complex relativeness scenarios, like this one:   R:\Repositories\swycartographer\res\msys\..\..\mod   R:\Repositories\swycartographer\res\..\mod   R:\Repositories\swycartographer\mod

--- res/msys/module_info.py
+++ res/msys/module_info.py
@@ -1,4 +1,4 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
-export_dir = "../mod"    
+export_dir = "../../mod"    
 #export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
